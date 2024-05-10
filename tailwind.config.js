c69f92742b890b016c2cd105d9327fd17a2390c7
tomas pereira <tomaspereira2k@gmail.com>
2024-05-09 23:57:42 -0300

    fix de un bug minimo

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html",
-            "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+
+  ],
   theme: {
     extend: {
       fontFamily: {
         'sans': "'Noto Sans JP', sans-serif"
       },
-      maxWidth: {
-        'custom-description': "29ch",
-        'custom-contact': "35ch",
-        'custom-contact-lg': "45ch",
-        'custom-title-lg': "20ch",
-        'custom-subtitle-lg': "55ch"
-      },
-      backgroundImage: {
-        'custom-bg': "url('./nnnoise.svg')"
-      }
     },
   },
   plugins: [],
 }
-
